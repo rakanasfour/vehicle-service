48e8eed4dc8e3054804729c716b18fbd02ee7e42
rakanasfour <rjor4@hotmail.com>
2024-05-02 23:11:08 -0700

    my last push for the project

--- src/main/java/com/vservice/model/ServiceModel.java
+++ src/main/java/com/vservice/model/ServiceModel.java
@@ -1,8 +1,7 @@
 package com.vservice.model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
-
-
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,82 +9,117 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 
-//@Document(value="service")
-//@DynamoDBTable(tableName = "vehicle-service")
 
 @Table(name="service")
 @Entity
 public class ServiceModel {
 	@Id
 	private int id;
-	@Column(name="date")
-	private Date date;
+	@Column(name="owner")
+	private String owner;
+	
+	@Column(name="brand")
+	private String brand;
+	
+	@Column(name="model")
+	private String model;
+	
 	@Column(name="type")
-	private String type;
+	private ServiceEnum type;
+	
 	@Column(name="cost")
 	private int cost;
-	@Column(name="depreciation")
-	private int depreciation;
 	
-  //  @DynamoDBHashKey(attributeName = "id")
+	@Column(name="date")
+	private Date date;
+
+	public ServiceModel(int id, String owner, String brand, String model, ServiceEnum type, int cost, Date date) {
+		super();
+		this.id = id;
+		this.owner = owner;
+		this.brand = brand;
+		this.model = model;
+		this.type = type;
+		this.cost = cost;
+		this.date = date;
+	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-  ///  @DynamoDBAttribute(attributeName = "Name")
 
-	public Date getDate() {
-		return date;
+	public String getOwner() {
+		return owner;
 	}
-	public void setDate(Date date) {
-		this.date = date;
+
+	public void setOwner(String owner) {
+		this.owner = owner;
 	}
-  //  @DynamoDBAttribute(attributeName = "Type")
-	public String getType() {
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public ServiceEnum getType() {
 		return type;
 	}
-	public void setType(String type) {
+
+	public void setType(ServiceEnum type) {
 		this.type = type;
 	}
-   // @DynamoDBAttribute(attributeName = "Cost")
 
 	public int getCost() {
 		return cost;
 	}
+
 	public void setCost(int cost) {
 		this.cost = cost;
 	}
-   // @DynamoDBAttribute(attributeName = "Depreciation")
 
-	public int getDepreciation() {
-		return depreciation;
+	public String getDate() {
+	    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+	    return formatter.format(date);
 	}
-	public void setDepreciation(int depreciation) {
-		this.depreciation = depreciation;
-	}
-	public ServiceModel(int id, Date date, String type, int cost, int depreciation) {
-		super();
-		this.id = id;
+
+
+	public void setDate(Date date) {
 		this.date = date;
-		this.type = type;
-		this.cost = cost;
-		this.depreciation = depreciation;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "ServiceModel [id=" + id + ", owner=" + owner + ", brand=" + brand + ", model=" + model + ", type="
+				+ type + ", cost=" + cost + ", date=" + date + "]";
+	}
+
 	public ServiceModel() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	@Override
-	public String toString() {
-		return "ServiceModel [id=" + id + ", date=" + date + ", type=" + type + ", cost=" + cost + ", depreciation="
-				+ depreciation + "]";
-	}
+
 	
 	
+
+	
+	
+	
+
 	
 	
 	
